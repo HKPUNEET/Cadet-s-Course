dd2305d065a2ae5b49354f7294fdaabdce111311
HKPUNEET <gowdakempe4@gmail.com>
2025-08-30 16:57:09 +0530

    added sitemap and robots

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://cadet-s-course.vercel.app',
+    generateRobotsTxt: true, // <== this creates robots.txt automatically
+}
